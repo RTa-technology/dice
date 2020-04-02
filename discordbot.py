524997c04ca24b6da146f89b80f4fa0f8e1020a6
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-03 03:47:53 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,11 @@
-from discord.ext import commands
 import os
 import traceback
+import random
+from discord.ext import commands as rta
 
-bot = commands.Bot(command_prefix='/')
+
+bot = rta.Bot(command_prefix='$')#, help_command=JapaneseHelpCommand()
+
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -12,10 +15,34 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.command()
+@bot.command(name="ping")
 async def ping(ctx):
-    await ctx.send('pong')
+        await ctx.send('pong')
+
+@bot.command(name="dice")
+async def dice(ctx, dice: str):
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    msg = f"{ctx.author.mention}\n" + result 
+    await ctx.send(msg)
+
+
+
+#class JapaneseHelpCommand(commands.DefaultHelpCommand):
+#   def __init__(self):
+#        super().__init__()
+#        self.commands_heading = "コマンド:"
+#        self.no_category = "その他"
+#        self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
+#
+#    def get_ending_note(self):
+#        return (f"各コマンドの説明: $help <コマンド名>\n"
+#                f"各カテゴリの説明: $help <カテゴリ名>\n")
+#
 
 
 bot.run(token)
