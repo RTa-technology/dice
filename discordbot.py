bff71cba277ec6ccb13ec1a688264911a6b2d489
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-03 04:22:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,13 +10,6 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
-async def on_ready(): # 起動した1度のみ呼ばれる
-    await client.change_presence(activity=discord.Game(name='!d NdN'))
-
-
-
-
-@bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
