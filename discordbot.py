710018c8ddabdb8599899f73793d805e06fadbef
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-05 18:16:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -121,16 +121,16 @@ async def dp(ctx, dice: str):
     await ctx.send(f"{ctx.author.mention}")
     await ctx.send(embed=embed)
 
-class JapaneseHelpCommand(commands.DefaultHelpCommand):
-  def __init__(self):
-       super().__init__()
-       self.commands_heading = "コマンド:"
-       self.no_category = "その他"
-       self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
+# class JapaneseHelpCommand(commands.DefaultHelpCommand):
+#   def __init__(self):
+#        super().__init__()
+#        self.commands_heading = "コマンド:"
+#        self.no_category = "その他"
+#        self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
 
-   def get_ending_note(self):
-       return (f"各コマンドの説明: $help <コマンド名>\n"
-               f"各カテゴリの説明: $help <カテゴリ名>\n")
+#    def get_ending_note(self):
+#        return (f"各コマンドの説明: $help <コマンド名>\n"
+#                f"各カテゴリの説明: $help <カテゴリ名>\n")
 
 
 
