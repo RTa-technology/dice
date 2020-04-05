0c94e7674eba9f0208bc12e0d854b920cee36c95
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-05 18:14:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -65,47 +65,58 @@ async def dp(ctx, dice: str):
         rolls = int(rolls)
         limit = int(limit)
         plus = int(plus)
-
+        judge_limit = int(judge_limit)
+        
+        result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+        mappedData = map(int, result.split(","))
+        output = list(mappedData)
+        sumresult = sum(output)
+        sumresult = sumresult + plus
+            if sumresult < judge_limit:
+                sumresult = f"{str(sumresult)} < {str(judge_limit)} => 成功"
+            elif sumresult >= judge_limit :
+                sumresult = f"{str(sumresult)} >= {str(judge_limit)} => 失敗"
+            else:
+                sumresult = sumresult
+                return
     elif dice in l2:
         rolls, str1 = map(str, dice.split('d'))
         limit, plus = map(str, str1.split(' +'))
         rolls = int(rolls)
         limit = int(limit)
         plus = int(plus)
-
+        result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+        mappedData = map(int, result.split(","))
+        output = list(mappedData)
+        sumresult = sum(output)
+        sumresult = sumresult + plus
+        
     elif dice in l3:
         rolls, str2 = map(str, dice.split('d'))
         plus, judge_limit = map(str, str2.split(' <'))
         rolls = int(rolls)
         limit = int(limit)
-
+        judge_limit = int(judge_limit)
+            if sumresult < judge_limit:
+                sumresult = f"{str(sumresult)} < {str(judge_limit)} => 成功"
+            elif sumresult >= judge_limit :
+                sumresult = f"{str(sumresult)} >= {str(judge_limit)} => 失敗"
+            else:
+                sumresult = sumresult
+                return
+        
     else:
         rolls, limit = map(str, dice.split('d'))
         rolls = int(rolls)
         limit = int(limit)
-    # try:
-    #     rolls, limit = map(int, dice.split('d'))
-    # except Exception:
-    #     await ctx.send('!dice NdNの書式で入力')
-#         return
-    # result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    # mappedData = map(int, result.split(","))
-    # output = list(mappedData)
-    # sumresult = sum(output)
-    judge_limit = int(judge_limit)
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    mappedData = map(int, result.split(","))
-    output = list(mappedData)
-    sumresult = sum(output)
-    sumresult = sumresult + plus
-        if sumresult < judge_limit:
-            result = str(sumresult) + "<" + str(judge_limit) + "=> 成功"
-        elif sumresult >= judge_limit :
-            result = str(sumresult) + ">=" + str(judge_limit) + "=> 失敗"
-        else:
-            result = result
+        result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+        mappedData = map(int, result.split(","))
+        output = list(mappedData)
+        sumresult = sum(output)
+        return
+    
 
-    embed = discord.Embed(title=sumresult ,description=f"{ctx.author.name}\n{ctx.message.content}",color=discord.Colour.from_rgb(255,0,0))
+    embed = discord.Embed(title=f"{sumresult}" ,description=f"{ctx.author.name}\n{ctx.message.content}",color=discord.Colour.from_rgb(255,0,0))
     embed.set_author(name=result)
     await ctx.send(f"{ctx.author.mention}")
     await ctx.send(embed=embed)
