bc4ee4b188e6c280324b9fdaa3d019d973c2d7f0
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-05 19:51:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -55,12 +55,8 @@ async def dice(ctx, dice: str):
 @bot.command(name="dp")
 async def dp(ctx, dice: str):
     """{n}d{n}+kの書式で入力"""
-    try:
         rolls, str1 = map(str, dice.split('d'))
         limit, plus =map(int, str1.split('+'))
-    except Exception:
-        await ctx.send('!d NdN+kの書式で入力')
-        return
     rolls = int(rolls)
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     mappedData = map(int, result.split(","))
