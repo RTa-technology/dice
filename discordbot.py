9a40a728e717ef97e8a074ddec0ec25e4e496ef3
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-05 18:02:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -52,75 +52,75 @@ async def dice(ctx, dice: str):
     await ctx.send(f"{ctx.author.mention}")
     await ctx.send(embed=embed)
 
-# @bot.command(name="dp")
-# async def dp(ctx, dice: str):
-#     l1 = [+, <] #1d100 +6 <50
-#     l2 = +
-#     l3 = <
-#     """{n}d{n}の書式で入力"""
-#     if dice in l1:
-#         rolls, str1 = map(str, dice.split('d'))
-#         limit, str2 = map(str, str1.split(' +'))
-#         plus, judge_limit = map(str, str2.split(' <'))
-#         rolls = int(rolls)
-#         limit = int(limit)
-#         plus = int(plus)
+@bot.command(name="dp")
+async def dp(ctx, dice: str):
+    l1 = ['+', '<'] #1d100 +6 <50
+    l2 = '+'
+    l3 = '<'
+    """{n}d{n}の書式で入力"""
+    if dice in l1:
+        rolls, str1 = map(str, dice.split('d'))
+        limit, str2 = map(str, str1.split(' +'))
+        plus, judge_limit = map(str, str2.split(' <'))
+        rolls = int(rolls)
+        limit = int(limit)
+        plus = int(plus)
 
-#     elif dice in l2:
-#         rolls, str1 = map(str, dice.split('d'))
-#         limit, plus = map(str, str1.split(' +'))
-#         rolls = int(rolls)
-#         limit = int(limit)
-#         plus = int(plus)
+    elif dice in l2:
+        rolls, str1 = map(str, dice.split('d'))
+        limit, plus = map(str, str1.split(' +'))
+        rolls = int(rolls)
+        limit = int(limit)
+        plus = int(plus)
 
-#     elif dice in l3:
-#         rolls, str2 = map(str, dice.split('d'))
-#         plus, judge_limit = map(str, str2.split(' <'))
-#         rolls = int(rolls)
-#         limit = int(limit)
+    elif dice in l3:
+        rolls, str2 = map(str, dice.split('d'))
+        plus, judge_limit = map(str, str2.split(' <'))
+        rolls = int(rolls)
+        limit = int(limit)
 
-#     else:
-#         rolls, limit = map(str, dice.split('d'))
-#         rolls = int(rolls)
-#         limit = int(limit)
-#     # try:
-#     #     rolls, limit = map(int, dice.split('d'))
-#     # except Exception:
-#     #     await ctx.send('!dice NdNの書式で入力')
-# #         return
-#     # result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-#     # mappedData = map(int, result.split(","))
-#     # output = list(mappedData)
-#     # sumresult = sum(output)
-#     judge_limit = int(judge_limit)
-#     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-#     mappedData = map(int, result.split(","))
-#     output = list(mappedData)
-#     sumresult = sum(output)
-#     sumresult = sumresult + plus
-#         if sumresult < judge_limit:
-#             result = str(sumresult) + "<" + str(judge_limit) + "=> 成功"
-#         elif sumresult >= judge_limit :
-#             result = str(sumresult) + ">=" + str(judge_limit) + "=> 失敗"
-#         else:
-#             result = result
+    else:
+        rolls, limit = map(str, dice.split('d'))
+        rolls = int(rolls)
+        limit = int(limit)
+    # try:
+    #     rolls, limit = map(int, dice.split('d'))
+    # except Exception:
+    #     await ctx.send('!dice NdNの書式で入力')
+#         return
+    # result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    # mappedData = map(int, result.split(","))
+    # output = list(mappedData)
+    # sumresult = sum(output)
+    judge_limit = int(judge_limit)
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    mappedData = map(int, result.split(","))
+    output = list(mappedData)
+    sumresult = sum(output)
+    sumresult = sumresult + plus
+        if sumresult < judge_limit:
+            result = str(sumresult) + "<" + str(judge_limit) + "=> 成功"
+        elif sumresult >= judge_limit :
+            result = str(sumresult) + ">=" + str(judge_limit) + "=> 失敗"
+        else:
+            result = result
 
-#     embed = discord.Embed(title=sumresult ,description=f"{ctx.author.name}\n{ctx.message.content}",color=discord.Colour.from_rgb(255,0,0))
-#     embed.set_author(name=result)
-#     await ctx.send(f"{ctx.author.mention}")
-#     await ctx.send(embed=embed)
+    embed = discord.Embed(title=sumresult ,description=f"{ctx.author.name}\n{ctx.message.content}",color=discord.Colour.from_rgb(255,0,0))
+    embed.set_author(name=result)
+    await ctx.send(f"{ctx.author.mention}")
+    await ctx.send(embed=embed)
 
-#class JapaneseHelpCommand(commands.DefaultHelpCommand):
-#   def __init__(self):
-#        super().__init__()
-#        self.commands_heading = "コマンド:"
-#        self.no_category = "その他"
-#        self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
-#
-#    def get_ending_note(self):
-#        return (f"各コマンドの説明: $help <コマンド名>\n"
-#                f"各カテゴリの説明: $help <カテゴリ名>\n")
-#
+class JapaneseHelpCommand(commands.DefaultHelpCommand):
+  def __init__(self):
+       super().__init__()
+       self.commands_heading = "コマンド:"
+       self.no_category = "その他"
+       self.command_attrs["help"] = "コマンド一覧と簡単な説明を表示"
+
+   def get_ending_note(self):
+       return (f"各コマンドの説明: $help <コマンド名>\n"
+               f"各カテゴリの説明: $help <カテゴリ名>\n")
+
 
 
 bot.run(token)
