8f4d8cced29e8399f4ec31550e5c8938902478d0
RTa-technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-05 20:09:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -105,7 +105,7 @@ async def dj(ctx, dice: str):
 @bot.command(name="di")
 async def di(ctx, dice: str):
     """{n}d{n}の書式で入力"""
-    c = ["+"]
+    c = ["¥+"]
     if dice in c:
         rolls, str1 = map(str, dice.split('d'))
         limit, plus =map(int, str1.split('+'))
