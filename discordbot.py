ec29cd00bec1142ecbb9c7ed2493a2d850995a17
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-09 01:15:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -155,58 +155,54 @@ async def dj(ctx, dice: str):
 
     
 @bot.command(name="s")
-async def s(ctx: str):
-	global STR_4176
-	global DEX_4176
-	global INT_4176
-	global CON_4176
-	global APP_4176
-	global POW_4176
-	global SIZ_4176
-	global SAN_4176
-	global EDU_4176
-	global HP_4176
-
-	global STR_4091
-	global DEX_4091
-	global INT_4091
-	global CON_4091
-	global APP_4091
-	global POW_4091
-	global SIZ_4091
-	global SAN_4091
-	global EDU_4091
-	global HP_4091
-
-	global STR_4560
-	global DEX_4560
-	global INT_4560
-	global CON_4560
-	global APP_4560
-	global POW_4560
-	global SIZ_4560
-	global SAN_4560
-	global EDU_4560
-	global HP_4560
-
-	global STR_864
-	global DEX_864
-	global INT_864
-	global CON_864
-	global APP_864
-	global POW_864
-	global SIZ_864
-	global SAN_864
-	global EDU_864
-	global HP_864
-    
-    if ctx.author.id == 4176:    
-        msg1 = f"STR:{STR_4176}\nDEX:{DEX_4176}\nINT:{INT_4176}\nCON:{CON_4176}\nAPP:{APP_4176}\nPOW:{POW_4176}\nSIZ:{SIZ_4176}\nSAN:{SAN_4176}\nEDU:{EDU_4176}\nHP:{HP_4176}"
+async def s(ctx: str):    
+    if ctx.author.id == 4176:  
+        global STR_4176
+        global DEX_4176
+        global INT_4176
+        global CON_4176
+        global APP_4176
+        global POW_4176
+        global SIZ_4176
+        global SAN_4176
+        global EDU_4176
+        global HP_4176
+	msg1 = f"STR:{STR_4176}\nDEX:{DEX_4176}\nINT:{INT_4176}\nCON:{CON_4176}\nAPP:{APP_4176}\nPOW:{POW_4176}\nSIZ:{SIZ_4176}\nSAN:{SAN_4176}\nEDU:{EDU_4176}\nHP:{HP_4176}"
     elif ctx.author.id == 4091:
+        global STR_4091
+        global DEX_4091
+        global INT_4091
+        global CON_4091
+        global APP_4091
+        global POW_4091
+        global SIZ_4091
+        global SAN_4091
+        global EDU_4091
+        global HP_4091
         msg1 = f"STR:{STR_4091}\nDEX:{DEX_4091}\nINT:{INT_4091}\nCON:{CON_4091}\nAPP:{APP_4091}\nPOW:{POW_4091}\nSIZ:{SIZ_4091}\nSAN:{SAN_4091}\nEDU:{EDU_4091}\nHP:{HP_4091}"
     elif ctx.author.id == 4560:
+        global STR_4176
+        global DEX_4176
+        global INT_4176
+        global CON_4176
+        global APP_4176
+        global POW_4176
+        global SIZ_4176
+        global SAN_4176
+        global EDU_4176
+        global HP_4176        
         msg1 = f"STR:{STR_4560}\nDEX:{DEX_4560}\nINT:{INT_4560}\nCON:{CON_4560}\nAPP:{APP_4560}\nPOW:{POW_4560}\nSIZ:{SIZ_4560}\nSAN:{SAN_4560}\nEDU:{EDU_4560}\nHP:{HP_4560}"
     elif ctx.author.id == 864:
+        global STR_864
+        global DEX_864
+        global INT_864
+	global CON_864
+        global APP_864
+        global POW_864
+        global SIZ_864
+        global SAN_864
+        global EDU_864
+        global HP_864
         msg1 = f"STR:{STR_864}\nDEX:{DEX_864}\nINT:{INT_864}\nCON:{CON_864}\nAPP:{APP_864}\nPOW:{POW_864}\nSIZ:{SIZ_864}\nSAN:{SAN_864}\nEDU:{EDU_864}\nHP:{HP_864}"
     await ctx.send(msg1)
 
