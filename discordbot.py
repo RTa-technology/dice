c274f0659bcf4941101b23afcd8e6d9fe6e30b41
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-09 20:41:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,50 +11,61 @@ bot = rta.Bot(command_prefix='!')#, help_command=JapaneseHelpCommand()
 
 token = os.environ['DISCORD_BOT_TOKEN']
 
-#Lenz
-STR_4176 = 14
-DEX_4176 = 14
-INT_4176 = 13
-CON_4176 = 14
-APP_4176 = 10
-POW_4176 = 11
-SIZ_4176 = 10
-SAN_4176 = 55
-EDU_4176 = 15
+# #Lenz
+# STR_4176 = 14
+# DEX_4176 = 14
+# INT_4176 = 13
+# CON_4176 = 14
+# APP_4176 = 10
+# POW_4176 = 11
+# SIZ_4176 = 10
+# SAN_4176 = 55
+# EDU_4176 = 15
+# HP_4176 = 12
+# #rta
+# STR_4091 = 12
+# DEX_4091 = 12
+# INT_4091 = 11
+# CON_4091 = 7
+# APP_4091 = 7
+# POW_4091 = 11
+# SIZ_4091 = 12
+# SAN_4091 = 55
+# EDU_4091 = 9
+# HP_4091 = 9
+# #extra
+# STR_4560 = 18
+# DEX_4560 = 13
+# INT_4560 = 14
+# CON_4560 = 11
+# APP_4560 = 10
+# POW_4560 = 6
+# SIZ_4560 = 13
+# SAN_4560 = 30
+# EDU_4560 = 18
+# HP_4560 = 12
+# #konuma
+# STR_864 = 18
+# DEX_864 = 13
+# INT_864 = 14
+# CON_864 = 11
+# APP_864 = 10
+# POW_864 = 6
+# SIZ_864 = 13
+# SAN_864 = 30
+# EDU_864 = 18
+# HP_864 = 12
+
+    
 HP_4176 = 12
-#rta
-STR_4091 = 12
-DEX_4091 = 12
-INT_4091 = 11
-CON_4091 = 7
-APP_4091 = 7
-POW_4091 = 11
-SIZ_4091 = 12
-SAN_4091 = 55
-EDU_4091 = 9
-HP_4091 = 9
-#extra
-STR_4560 = 18
-DEX_4560 = 13
-INT_4560 = 14
-CON_4560 = 11
-APP_4560 = 10
-POW_4560 = 6
-SIZ_4560 = 13
-SAN_4560 = 30
-EDU_4560 = 18
-HP_4560 = 12
-#konuma
-STR_864 = 18
-DEX_864 = 13
-INT_864 = 14
-CON_864 = 11
-APP_864 = 10
-POW_864 = 6
-SIZ_864 = 13
-SAN_864 = 30
-EDU_864 = 18
-HP_864 = 12
+HP_4091 = 11
+HP_4560 = 55
+MP_4176 = 16
+MP_4091 = 6
+MP_4560 = 30
+SA_4176 = 12
+SA_4091 = 11
+SA_4560 = 55
 
 
 
@@ -152,56 +163,94 @@ async def dj(ctx, dice: str):
     await ctx.send(f"{ctx.author.mention}")
     await ctx.send(embed=embed)
 
+
     
-@bot.command(name="s4176")
+
+
+    
+@bot.command(name="l4176")
 async def s4176(ctx: str):
-    global STR_4176
-    global DEX_4176
-    global INT_4176
-    global CON_4176
-    global APP_4176
-    global POW_4176
-    global SIZ_4176
-    global SAN_4176
-    global EDU_4176
     global HP_4176
-    msg0 = f"STR:{STR_4176}\nDEX:{DEX_4176}\nINT:{INT_4176}\nCON:{CON_4176}\nAPP:{APP_4176}\nPOW:{POW_4176}\nSIZ:{SIZ_4176}\nSAN:{SAN_4176}\nEDU:{EDU_4176}\nHP:{HP_4176}"
-    await ctx.send(msg0)
+    global MP_4176
+    global SA_4176
+
+    msg = f"耐久力 {HP_4176}/12. MP {MP_4176}/11.  正気度 {SA_4176}/99."
+    await ctx.send(msg)
     
-@bot.command(name="shp")
+@bot.command(name="k0864")
 async def s4176(ctx,stu: str):
-    global STR_4176
-    global DEX_4176
-    global INT_4176
-    global CON_4176
-    global APP_4176
-    global POW_4176
-    global SIZ_4176
-    global SAN_4176
-    global EDU_4176
     global HP_4176
+    global MP_4176
+    global SA_4176
     global HP_4091
-    global HP_4560  
-    status, plus = map(str, stu.split('+'))
+    global MP_4091
+    global SA_4091
+    global HP_4560
+    global MP_4560
+    global SA_4560
+
+
+    msg = f"トイ=ウォーターズ(パイロット)\n耐久力 {HP_4176}/12. MP {MP_4176}/11.  正気度 {SAN_4176}/99.\n\nケビン=アンダーソン(スポーツ選手)\n耐久力 {HP_4560}/16. MP {MP_4560}/6. 正気度 {SA_4560}/99.\n\nルイ＝ビール(警官)\n耐久値 {SAN_4091}/12. MP {MP_4091}/11. 正気度 {SA_4091}/99."
+
+    await ctx.send(msg)
+    
+@bot.command(name="e4560")
+async def s4176(ctx: str):
+    global HP_4560
+    global MP_4560
+    global SA_4560
+
+    msg = f"耐久力 {HP_4560}/12. MP {MP_4560}/11.  正気度 {SA_4560}/99."
+    await ctx.send(msg)
+
+@bot.command(name="r4091")
+async def s4176(ctx: str):
+    global HP_4091
+    global MP_4091
+    global SA_4091
+
+    msg = f"耐久力 {HP_4091}/12. MP {MP_4091}/11.  正気度 {SA_4091}/99."
+    await ctx.send(msg)
+
+@bot.command(name="rp4091")
+async def s4176(ctx, stu: str):
+    global HP_4091
+    global MP_4091
+    global SA_4091
+    
+    states, plus = map(str, stu.split('+'))
     plus = int(plus)
-    a_id = ctx.author.id
-    if a_id == 4176:
-        hp = HP_4176
-        HP_4176 = hp + plus
-        msg0 = f"STR:{STR_4176}\nDEX:{DEX_4176}\nINT:{INT_4176}\nCON:{CON_4176}\nAPP:{APP_4176}\nPOW:{POW_4176}\nSIZ:{SIZ_4176}\nSAN:{SAN_4176}\nEDU:{EDU_4176}\nHP:{HP_4176}"
-    elif a_id == 4091:
-        hp = HP_4091
-        HP_4091 = hp + plus
-        msg0 = f"STR:{STR_4091}\nDEX:{DEX_4091}\nINT:{INT_4091}\nCON:{CON_4091}\nAPP:{APP_4091}\nPOW:{POW_4091}\nSIZ:{SIZ_4091}\nSAN:{SAN_4091}\nEDU:{EDU_4091}\nHP:{HP_4091}"
-    elif a_id == 4560:
-        hp = HP_4560
-        HP_4560 = hp + plus
-        msg0 = f"STR:{STR_4560}\nDEX:{DEX_4560}\nINT:{INT_4560}\nCON:{CON_4560}\nAPP:{APP_4560}\nPOW:{POW_4560}\nSIZ:{SIZ_4560}\nSAN:{SAN_4560}\nEDU:{EDU_4560}\nHP:{HP_4560}"
-    await ctx.send(msg0)
-    
-    
-# @bot.command(name="sp4176")
-# async def sp4176(ctx, stu: str):
+    if states == "hp":
+        hp = HP_4091 + plus
+        HP_4091 = hp
+        msg1 = f"HPを+{plus}しました。"
+    elif states == "mp":
+        mp = MP_4091 + plus
+        MP_4091 = mp
+        msg1 = f"MPを+{plus}しました。"
+    elif states == "san":
+        san = SA_4091 + plus
+        SA_4091 == san
+        msg1 = f"SANを+{plus}しました。"
+    an = f"現在のルイ＝ビールのステータスを表示します。"
+    msg = f"ルイ＝ビール(警官)\n耐久値 {SAN_4091}/12. MP {MP_4091}/11. 正気度 {SA_4091}/99."
+    mention= f"<@{ctx.author.id}>"
+    embed = discord.Embed(title=an ,description=f"{msg1}\n結果:\n{msg}",color=discord.Colour.from_rgb(0,255,0))
+#     embed.set_author(name=firesult)
+    await ctx.send(f"{ctx.author.mention}")
+    await ctx.send(embed=embed)
+
+
+@bot.command(name="rm4091")
+async def s4176(ctx: str):
+    global HP_4091
+    global MP_4091
+    global SA_4091
+
+    msg = f"耐久力 {HP_4091}/12. MP {MP_4091}/11.  正気度 {SA_4091}/99."
+    await ctx.send(msg)
+# @bot.command(name="s4176")
+# async def s4176(ctx: str):
 #     global STR_4176
 #     global DEX_4176
 #     global INT_4176
@@ -212,55 +261,102 @@ async def s4176(ctx,stu: str):
 #     global SAN_4176
 #     global EDU_4176
 #     global HP_4176
-#     st = STR_4176
-#     dex = DEX_4176
-#     in =  INT_4176
-#     con = CON_4176
-#     app = APP_4176
-#     pw = POW_4176
-#     siz = SIZ_4176
-#     san = SAN_4176
-#     edu = EDU_4176
-#     hp = HP_4176
-#     status, plus = map(int, stu.split('+'))
-#     if status == 1:
-#         hp = hp + plus
-#         HP_4176 = hp
-#     else:
-#         HP_4176 = HP_4176
 #     msg0 = f"STR:{STR_4176}\nDEX:{DEX_4176}\nINT:{INT_4176}\nCON:{CON_4176}\nAPP:{APP_4176}\nPOW:{POW_4176}\nSIZ:{SIZ_4176}\nSAN:{SAN_4176}\nEDU:{EDU_4176}\nHP:{HP_4176}"
 #     await ctx.send(msg0)
     
-@bot.command(name="s4091")
-async def s4091(ctx: str):
-    global STR_4091
-    global DEX_4091
-    global INT_4091
-    global CON_4091
-    global APP_4091
-    global POW_4091
-    global SIZ_4091
-    global SAN_4091
-    global EDU_4091
-    global HP_4091
-    msg1 = f"STR:{STR_4091}\nDEX:{DEX_4091}\nINT:{INT_4091}\nCON:{CON_4091}\nAPP:{APP_4091}\nPOW:{POW_4091}\nSIZ:{SIZ_4091}\nSAN:{SAN_4091}\nEDU:{EDU_4091}\nHP:{HP_4091}"
-    await ctx.send(msg1)
+# @bot.command(name="shp")
+# async def s4176(ctx,stu: str):
+#     global STR_4176
+#     global DEX_4176
+#     global INT_4176
+#     global CON_4176
+#     global APP_4176
+#     global POW_4176
+#     global SIZ_4176
+#     global SAN_4176
+#     global EDU_4176
+#     global HP_4176
+#     global HP_4091
+#     global HP_4560  
+#     status, plus = map(str, stu.split('+'))
+#     plus = int(plus)
+#     a_id = ctx.author.id
+#     if a_id == 4176:
+#         hp = HP_4176
+#         HP_4176 = hp + plus
+#         msg0 = f"STR:{STR_4176}\nDEX:{DEX_4176}\nINT:{INT_4176}\nCON:{CON_4176}\nAPP:{APP_4176}\nPOW:{POW_4176}\nSIZ:{SIZ_4176}\nSAN:{SAN_4176}\nEDU:{EDU_4176}\nHP:{HP_4176}"
+#     elif a_id == 4091:
+#         hp = HP_4091
+#         HP_4091 = hp + plus
+#         msg0 = f"STR:{STR_4091}\nDEX:{DEX_4091}\nINT:{INT_4091}\nCON:{CON_4091}\nAPP:{APP_4091}\nPOW:{POW_4091}\nSIZ:{SIZ_4091}\nSAN:{SAN_4091}\nEDU:{EDU_4091}\nHP:{HP_4091}"
+#     elif a_id == 4560:
+#         hp = HP_4560
+#         HP_4560 = hp + plus
+#         msg0 = f"STR:{STR_4560}\nDEX:{DEX_4560}\nINT:{INT_4560}\nCON:{CON_4560}\nAPP:{APP_4560}\nPOW:{POW_4560}\nSIZ:{SIZ_4560}\nSAN:{SAN_4560}\nEDU:{EDU_4560}\nHP:{HP_4560}"
+#     await ctx.send(msg0)
+    
+    
+# # @bot.command(name="sp4176")
+# # async def sp4176(ctx, stu: str):
+# #     global STR_4176
+# #     global DEX_4176
+# #     global INT_4176
+# #     global CON_4176
+# #     global APP_4176
+# #     global POW_4176
+# #     global SIZ_4176
+# #     global SAN_4176
+# #     global EDU_4176
+# #     global HP_4176
+# #     st = STR_4176
+# #     dex = DEX_4176
+# #     in =  INT_4176
+# #     con = CON_4176
+# #     app = APP_4176
+# #     pw = POW_4176
+# #     siz = SIZ_4176
+# #     san = SAN_4176
+# #     edu = EDU_4176
+# #     hp = HP_4176
+# #     status, plus = map(int, stu.split('+'))
+# #     if status == 1:
+# #         hp = hp + plus
+# #         HP_4176 = hp
+# #     else:
+# #         HP_4176 = HP_4176
+# #     msg0 = f"STR:{STR_4176}\nDEX:{DEX_4176}\nINT:{INT_4176}\nCON:{CON_4176}\nAPP:{APP_4176}\nPOW:{POW_4176}\nSIZ:{SIZ_4176}\nSAN:{SAN_4176}\nEDU:{EDU_4176}\nHP:{HP_4176}"
+# #     await ctx.send(msg0)
+    
+# @bot.command(name="s4091")
+# async def s4091(ctx: str):
+#     global STR_4091
+#     global DEX_4091
+#     global INT_4091
+#     global CON_4091
+#     global APP_4091
+#     global POW_4091
+#     global SIZ_4091
+#     global SAN_4091
+#     global EDU_4091
+#     global HP_4091
+#     msg1 = f"STR:{STR_4091}\nDEX:{DEX_4091}\nINT:{INT_4091}\nCON:{CON_4091}\nAPP:{APP_4091}\nPOW:{POW_4091}\nSIZ:{SIZ_4091}\nSAN:{SAN_4091}\nEDU:{EDU_4091}\nHP:{HP_4091}"
+#     await ctx.send(msg1)
  
 
-@bot.command(name="s4560")
-async def s4560(ctx: str):
-    global STR_4560
-    global DEX_4560
-    global INT_4560
-    global CON_4560
-    global APP_4560
-    global POW_4560
-    global SIZ_4560
-    global SAN_4560
-    global EDU_4560
-    global HP_4560
-    msg2 = f"STR:{STR_4560}\nDEX:{DEX_4560}\nINT:{INT_4560}\nCON:{CON_4560}\nAPP:{APP_4560}\nPOW:{POW_4560}\nSIZ:{SIZ_4560}\nSAN:{SAN_4560}\nEDU:{EDU_4560}\nHP:{HP_4560}"
-    await ctx.send(msg2)
+# @bot.command(name="s4560")
+# async def s4560(ctx: str):
+#     global STR_4560
+#     global DEX_4560
+#     global INT_4560
+#     global CON_4560
+#     global APP_4560
+#     global POW_4560
+#     global SIZ_4560
+#     global SAN_4560
+#     global EDU_4560
+#     global HP_4560
+#     msg2 = f"STR:{STR_4560}\nDEX:{DEX_4560}\nINT:{INT_4560}\nCON:{CON_4560}\nAPP:{APP_4560}\nPOW:{POW_4560}\nSIZ:{SIZ_4560}\nSAN:{SAN_4560}\nEDU:{EDU_4560}\nHP:{HP_4560}"
+#     await ctx.send(msg2)
  
 
 #     global STR_4176
