1c47dd6eaa3c9828253d6de5b420473c6656acd4
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-09 16:51:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,12 +11,6 @@ bot = rta.Bot(command_prefix='!')#, help_command=JapaneseHelpCommand()
 
 token = os.environ['DISCORD_BOT_TOKEN']
 
-# def has_any_role():
-#     async def predicate(ctx):
-#         if len(ctx.author.roles) > 1:
-#             return True
-#     return commands.check(predicate)
-
 #Lenz
 STR_4176 = 14
 DEX_4176 = 14
@@ -326,86 +320,6 @@ async def s4560(ctx: str):
 #         await ctx.send(msg3)
 #     else:
 # 	return
-
-
-# @bot.command(aliases=['cnt'])
-# @has_any_role()
-# async def count(self, ctx, num: typing.Optional[int] = 0):
-#     if num == 0:
-#         await ctx.send("引数を正しく入力してください")
-#         return
-
-#     msg = await ctx.send(f"{ctx.author.mention}\nリアクション集計を行います: 目標リアクション数 ** {num} **\n本メッセージにリアクションをつけてください")
-#     today = datetime.today()
-#     now = (today + timedelta(minutes=num)
-#            ).strftime('%Y-%m-%d %H:%M:%S')
-#     self.reaction_dict[str(msg.id)] = {
-#         "cnt": num,
-#         "author": ctx.author.mention,
-#         "reaction_sum": 0,
-#         "channel": ctx.channel.id,
-#         "matte": 0,
-#         "time": now,
-#         "url": ctx.message.jump_url}
-#     self.dump_json(self.reaction_dict)
-
-# @bot.command(aliases=['ls'])
-# @has_any_role()
-# async def list_data(self, ctx):
-#     if len(self.reaction_dict) == 0:
-#         await ctx.send("集計中のリアクションはありません")
-#     else:
-#         embed = discord.Embed(
-#             title="集計中のリアクションは以下の通りです",
-#             description=f"{len(self.reaction_dict)}件集計中",
-#             color=0xffffff)
-
-#         for num, i in enumerate(self.reaction_dict):
-#             auth = self.reaction_dict[i]["author"]
-#             time = self.reaction_dict[i]["time"]
-#             url = self.reaction_dict[i]["url"]
-#             reaction_sum = self.reaction_dict[i]["reaction_sum"]
-#             reaction_cnt = self.reaction_dict[i]["cnt"]
-
-#             if self.reaction_dict[i]["matte"] > 0:
-#                 matte = " **待って！**"
-#             else:
-#                 matte = ""
-
-#             embed.add_field(
-#                 name=f"{num+1}番目",
-#                 value=f"ID : {i} by : {auth} time : {time} prog : {reaction_sum}/{reaction_cnt}{matte}\n{url}",
-#                 inline=False)
-#         embed.set_footer(text="あんまり貯めないでね")
-#         await ctx.send(embed=embed)
-
-# @bot.Cog.listener()
-# async def on_raw_reaction_add(self, reaction):
-#     for msg_id in list(self.reaction_dict):
-#         if int(msg_id) == reaction.message_id:
-#             if "matte" in reaction.emoji.name:
-#                 self.reaction_dict[msg_id]["matte"] += 1
-#                 channel = self.bot.get_channel(reaction.channel_id)
-#                 msg = await channel.fetch_message(reaction.message_id)
-#                 await msg.edit(content=msg.content + "\n待ちます")
-#             else:
-#                 self.reaction_dict[msg_id]["reaction_sum"] += 1
-
-#             await self.judge_and_notice(msg_id)
-
-# @bot.Cog.listener()
-# async def on_raw_reaction_remove(self, reaction):
-#     for msg_id in list(self.reaction_dict):
-#         if int(msg_id) == reaction.message_id:
-#             if "matte" in reaction.emoji.name:
-#                 self.reaction_dict[msg_id]["matte"] -= 1
-#                 channel = self.bot.get_channel(reaction.channel_id)
-#                 msg = await channel.fetch_message(reaction.message_id)
-#                 await msg.edit(content=msg.content.replace("\n待ちます", "", 1))
-#             else:
-#                 self.reaction_dict[msg_id]["reaction_sum"] -= 1
-
-#             await self.judge_and_notice(msg_id)
 
 # @bot.command(name="di")
 # async def di(ctx, dice: str):
