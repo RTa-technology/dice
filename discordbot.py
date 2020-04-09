9ee0795f88a3492c9e2733b7beace471f38aeff9
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-09 20:59:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -168,17 +168,39 @@ async def dj(ctx, dice: str):
 
 
     
-@bot.command(name="l4176")
-async def s4176(ctx: str):
+@bot.command(name="l")
+async def l(ctx: str):
+    """!l(エル)の書式で入力:Lens"""
     global HP_4176
     global MP_4176
     global SA_4176
 
-    msg = f"耐久力 {HP_4176}/12. MP {MP_4176}/11.  正気度 {SA_4176}/99."
+    msg = f"トイ=ウォーターズ(パイロット)\n耐久力 {HP_4176}/12. MP {MP_4176}/11.  正気度 {SA_4176}/99."
     await ctx.send(msg)
     
-@bot.command(name="k0864")
-async def s4176(ctx,stu: str):
+@bot.command(name="e")
+async def e(ctx: str):
+    """!eの書式で入力:extra"""
+    global HP_4560
+    global MP_4560
+    global SA_4560
+
+    msg = f"ケビン=アンダーソン(スポーツ選手)\n耐久力 {HP_4560}/12. MP {MP_4560}/11.  正気度 {SA_4560}/99."
+    await ctx.send(msg)
+
+@bot.command(name="r")
+async def r(ctx: str):
+    """!rの書式で入力:rta"""
+    global HP_4091
+    global MP_4091
+    global SA_4091
+
+    msg = f"ルイ＝ビール(警官)\n耐久力 {HP_4091}/12. MP {MP_4091}/11.  正気度 {SA_4091}/99."
+    await ctx.send(msg)
+    
+@bot.command(name="k")
+async def k(ctx,stu: str):
+    """!kの書式で入力:konuma"""
     global HP_4176
     global MP_4176
     global SA_4176
@@ -194,26 +216,10 @@ async def s4176(ctx,stu: str):
 
     await ctx.send(msg)
     
-@bot.command(name="e4560")
-async def s4176(ctx: str):
-    global HP_4560
-    global MP_4560
-    global SA_4560
-
-    msg = f"耐久力 {HP_4560}/12. MP {MP_4560}/11.  正気度 {SA_4560}/99."
-    await ctx.send(msg)
-
-@bot.command(name="r4091")
-async def s4176(ctx: str):
-    global HP_4091
-    global MP_4091
-    global SA_4091
-
-    msg = f"耐久力 {HP_4091}/12. MP {MP_4091}/11.  正気度 {SA_4091}/99."
-    await ctx.send(msg)
-
-@bot.command(name="rp4091")
-async def s4176(ctx, stu: str):
+    
+@bot.command(name="rp")
+async def rp(ctx, stu: str):
+    """!rp {states}+{N}の書式で入力"""
     global HP_4091
     global MP_4091
     global SA_4091
@@ -240,15 +246,150 @@ async def s4176(ctx, stu: str):
     await ctx.send(f"{ctx.author.mention}")
     await ctx.send(embed=embed)
 
-
-@bot.command(name="rm4091")
-async def s4176(ctx: str):
+@bot.command(name="rm")
+async def rm(ctx, stu: str):
+    """!rm {states}-{N}の書式で入力"""
     global HP_4091
     global MP_4091
     global SA_4091
+    
+    states, minus = map(str, stu.split('-'))
+    minus = int(minus)
+    if states == "hp":
+        hp = HP_4091 - minus
+        HP_4091 = hp
+        msg1 = f"HPを-{minus}しました。"
+    elif states == "mp":
+        mp = MP_4091 - minus
+        MP_4091 = mp
+        msg1 = f"MPを-{minus}しました。"
+    elif states == "san":
+        san = SA_4091 - minus
+        SA_4091 == san
+        msg1 = f"SANを-{minus}しました。"
+    an = f"現在のルイ＝ビールのステータスを表示します。"
+    msg = f"ルイ＝ビール(警官)\n耐久値 {HP_4091}/12. MP {MP_4091}/11. 正気度 {SA_4091}/99."
+    mention= f"<@{ctx.author.id}>"
+    embed = discord.Embed(title=an ,description=f"{msg1}\n結果:\n{msg}",color=discord.Colour.from_rgb(0,255,0))
+#     embed.set_author(name=firesult)
+    await ctx.send(f"{ctx.author.mention}")
+    await ctx.send(embed=embed)
 
-    msg = f"耐久力 {HP_4091}/12. MP {MP_4091}/11.  正気度 {SA_4091}/99."
-    await ctx.send(msg)
+@bot.command(name="ep")
+async def ep(ctx, stu: str):
+    """!ep {states}+{N}の書式で入力"""
+    global HP_4560
+    global MP_4560
+    global SA_4560
+    
+    states, plus = map(str, stu.split('+'))
+    plus = int(plus)
+    if states == "hp":
+        hp = HP_4560 + plus
+        HP_4560 = hp
+        msg1 = f"HPを+{plus}しました。"
+    elif states == "mp":
+        mp = MP_4560 + plus
+        MP_4560 = mp
+        msg1 = f"MPを+{plus}しました。"
+    elif states == "san":
+        san = SA_4560 + plus
+        SA_4560 == san
+        msg1 = f"SANを+{plus}しました。"
+    an = f"現在のケビン=アンダーソンのステータスを表示します。"
+    msg = f"ケビン=アンダーソン(スポーツ選手)\n耐久値 {HP_4560}/16. MP {MP_4560}/6. 正気度 {SA_4560}/99."
+    mention= f"<@{ctx.author.id}>"
+    embed = discord.Embed(title=an ,description=f"{msg1}\n結果:\n{msg}",color=discord.Colour.from_rgb(0,255,0))
+#     embed.set_author(name=firesult)
+    await ctx.send(f"{ctx.author.mention}")
+    await ctx.send(embed=embed)
+
+@bot.command(name="em")
+async def em(ctx, stu: str):
+    """!em {states}-{N}の書式で入力"""
+    global HP_4560
+    global MP_4560
+    global SA_4560
+    
+    states, minus = map(str, stu.split('-'))
+    minus = int(minus)
+    if states == "hp":
+        hp = HP_4560 - minus
+        HP_4560 = hp
+        msg1 = f"HPを-{minus}しました。"
+    elif states == "mp":
+        mp = MP_4560 - minus
+        MP_4560 = mp
+        msg1 = f"MPを-{minus}しました。"
+    elif states == "san":
+        san = SA_4560 - minus
+        SA_4560 == san
+        msg1 = f"SANを-{minus}しました。"
+    an = f"現在のケビン=アンダーソンのステータスを表示します。"
+    msg = f"ケビン=アンダーソン(スポーツ選手)\n耐久値 {HP_4560}/16. MP {MP_4560}/6. 正気度 {SA_4560}/99." mention= f"<@{ctx.author.id}>"
+    embed = discord.Embed(title=an ,description=f"{msg1}\n結果:\n{msg}",color=discord.Colour.from_rgb(0,255,0))
+#     embed.set_author(name=firesult)
+    await ctx.send(f"{ctx.author.mention}")
+    await ctx.send(embed=embed)
+
+
+@bot.command(name="lp")
+async def lp(ctx, stu: str):
+    """!lp {states}+{N}の書式で入力"""
+    global HP_4176
+    global MP_4176
+    global SA_4176
+    
+    states, plus = map(str, stu.split('+'))
+    plus = int(plus)
+    if states == "hp":
+        hp = HP_4176 + plus
+        HP_4176 = hp
+        msg1 = f"HPを+{plus}しました。"
+    elif states == "mp":
+        mp = MP_4176 + plus
+        MP_4176 = mp
+        msg1 = f"MPを+{plus}しました。"
+    elif states == "san":
+        san = SA_4176 + plus
+        SA_4176 == san
+        msg1 = f"SANを+{plus}しました。"
+    an = f"現在のトイ=ウォーターズのステータスを表示します。"
+    msg = f"トイ=ウォーターズ(パイロット)\n耐久値 {HP_4176}/12. MP {MP_4176}/11. 正気度 {SA_4176}/99."
+    mention= f"<@{ctx.author.id}>"
+    embed = discord.Embed(title=an ,description=f"{msg1}\n結果:\n{msg}",color=discord.Colour.from_rgb(0,255,0))
+#     embed.set_author(name=firesult)
+    await ctx.send(f"{ctx.author.mention}")
+    await ctx.send(embed=embed)
+
+@bot.command(name="lm")
+async def lm(ctx, stu: str):
+    """!lm {states}-{N}の書式で入力"""
+    global HP_4176
+    global MP_4176
+    global SA_4176
+    
+    states, minus = map(str, stu.split('-'))
+    minus = int(minus)
+    if states == "hp":
+        hp = HP_4176 - minus
+        HP_4176 = hp
+        msg1 = f"HPを-{minus}しました。"
+    elif states == "mp":
+        mp = MP_4176 - minus
+        MP_4176 = mp
+        msg1 = f"MPを-{minus}しました。"
+    elif states == "san":
+        san = SA_4176 - minus
+        SA_4176 == san
+        msg1 = f"SANを-{minus}しました。"
+    an = f"現在のトイ=ウォーターズのステータスを表示します。"
+    msg = f"トイ=ウォーターズ(パイロット)\n耐久値 {HP_4176}/12. MP {MP_4176}/11. 正気度 {SA_4176}/99."    mention= f"<@{ctx.author.id}>"
+    embed = discord.Embed(title=an ,description=f"{msg1}\n結果:\n{msg}",color=discord.Colour.from_rgb(0,255,0))
+#     embed.set_author(name=firesult)
+    await ctx.send(f"{ctx.author.mention}")
+    await ctx.send(embed=embed)
+
 # @bot.command(name="s4176")
 # async def s4176(ctx: str):
 #     global STR_4176
