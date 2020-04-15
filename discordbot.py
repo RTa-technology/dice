d17775555821e75116b0c1333c4a8e65cdc05257
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-16 06:53:01 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -409,16 +409,7 @@ async def s0864(ctx,stu: str):
     
     a_id = ctx.author.id
     if a_id == 556772231011631104:
-        try:
-            states, plus = map(str, stu.split('+'))
-
-        except Exception:
-            an = f"現在の滝口 明夫のステータスを表示します。"
-            msg = f"滝口 明夫\n耐久値 {HP_0864}/10. MP {MP_0864}/12. 正気度 {SA_0864}/99."
-            embed = discord.Embed(title=an ,description=f"{msg}",color=discord.Colour.from_rgb(87,100,74))
-            await ctx.send(f"{ctx.author.mention}")
-            await ctx.send(embed=embed)    
-            return
+        states, plus = map(str, stu.split('+'))
         plus = int(plus)
         if states == "hp":
             hp = HP_0864 + plus
@@ -435,16 +426,7 @@ async def s0864(ctx,stu: str):
         an = f"現在の滝口 明夫のステータスを表示します。"
         msg = f"滝口 明夫\n耐久値 {HP_0864}/12. MP {MP_0864}/11. 正気度 {SA_0864}/99."
     elif a_id == 649984563292012545:
-        try:
-            states, plus = map(str, stu.split('+'))
-
-        except Exception:
-            an = f"現在の苑田 晋助のステータスを表示します。"
-            msg = f"苑田 晋助\n耐久値 {HP_0191}/12. MP {MP_0191}/15. 正気度 {SA_0191}/99."
-            embed = discord.Embed(title=an ,description=f"{msg}",color=discord.Colour.from_rgb(87,100,74))
-            await ctx.send(f"{ctx.author.mention}")
-            await ctx.send(embed=embed)    
-            return
+        states, plus = map(str, stu.split('+'))
         plus = int(plus)
         if states == "hp":
             hp = HP_0191 + plus
@@ -461,15 +443,7 @@ async def s0864(ctx,stu: str):
         an = f"現在の苑田 晋助を表示します。"
         msg = f"苑田 晋助\n耐久値 {HP_0191}/12. MP {MP_0191}/11. 正気度 {SA_0191}/99."
     elif a_id == 452095387990229002:
-        try:
-            states, plus = map(str, stu.split('+'))
-        except Exception:
-            an = f"現在の鈴木 耕一のステータスを表示します。"
-            msg = f"鈴木 耕一\n耐久力 {HP_8199}/12\11. MP {MP_8199}/14. 正気度 {SA_8199}/99."
-            embed = discord.Embed(title=an ,description=f"{msg}",color=discord.Colour.from_rgb(87,100,74))
-            await ctx.send(f"{ctx.author.mention}")
-            await ctx.send(embed=embed)    
-            return
+        states, plus = map(str, stu.split('+'))
         plus = int(plus)
         if states == "hp":
             hp = HP_8199 + plus
@@ -486,16 +460,8 @@ async def s0864(ctx,stu: str):
         an = f"現在の鈴木 耕一のステータスを表示します。"
         msg = f"鈴木 耕一\n耐久値 {HP_8199}/16. MP {MP_8199}/6. 正気度 {SA_8199}/99."
 #     elif a_id == 556772231011631104:
-#         try:
-#             pl_di, str1 = map(str, stu.split('&'))
-#             states, plus = map(str, str1.split('+'))
-#         except Exception:
-#             an = f"現在の全Playerのステータスを表示します。"
-#             msg = f"滝口 明夫\n耐久力 {HP_0864}/10. MP {MP_0864}/12.  正気度 {SA_0864}/99.\n\n鈴木 耕一\n耐久力 {HP_8199}/12\11. MP {MP_8199}/14. 正気度 {SA_8199}/99.\n\n苑田 晋助\n耐久値 {HP_0191}/12. MP {MP_0191}/15. 正気度 {SA_0191}/99."
-#             embed = discord.Embed(title=an ,description=msg,color=discord.Colour.from_rgb(87,100,74))
-#             await ctx.send(f"{ctx.author.mention}")
-#             await ctx.send(embed=embed)    
-#             return
+#       　pl_di, str1 = map(str, stu.split('&'))
+#         states, plus = map(str, stu.split('+'))
 #         plus = int(plus)
 #         if pl_di == "0864":
 #             if states == "hp":
