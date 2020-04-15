42f8493129af13d391b41e6d39efa731861499d2
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-16 05:17:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -392,6 +392,132 @@ async def lm(ctx, stu: str):
     await ctx.send(f"{ctx.author.mention}")
     await ctx.send(embed=embed)
 
+    
+    
+@bot.command(name="p")
+async def s0864(ctx,stu: str):
+
+    global HP_0864
+    global MP_0864
+    global SA_0864
+    global HP_0191
+    global MP_0191
+    global SA_0191
+    global HP_8199
+    global MP_8199
+    global SA_8199
+    
+    status, plus = map(str, stu.split('+'))
+    plus = int(plus)
+    a_id = ctx.author.id
+    if a_id == 294106055397474314:
+        states, plus = map(str, stu.split('+'))
+        plus = int(plus)
+        if states == "hp":
+            hp = HP_0864 + plus
+            HP_0864 = hp
+            msg1 = f"HPを+{plus}しました。"
+        elif states == "mp":
+            mp = MP_0864 + plus
+            MP_0864 = mp
+            msg1 = f"MPを+{plus}しました。"
+        elif states == "san":
+            san = SA_0864 + plus
+            SA_0864 = san
+            msg1 = f"SANを+{plus}しました。"
+        an = f"現在の滝口 明夫のステータスを表示します。"
+        msg = f"滝口 明夫\n耐久値 {HP_0864}/12. MP {MP_0864}/11. 正気度 {SA_0864}/99."
+    elif a_id == 649984563292012545:
+        states, plus = map(str, stu.split('+'))
+        plus = int(plus)
+        if states == "hp":
+            hp = HP_0191 + plus
+            HP_0191 = hp
+            msg1 = f"HPを+{plus}しました。"
+        elif states == "mp":
+            mp = MP_0191 + plus
+            MP_0191 = mp
+            msg1 = f"MPを+{plus}しました。"
+        elif states == "san":
+            san = SA_0191 + plus
+            SA_0191 = san
+            msg1 = f"SANを+{plus}しました。"
+        an = f"現在の苑田 晋助を表示します。"
+        msg = f"苑田 晋助\n耐久値 {HP_0191}/12. MP {MP_0191}/11. 正気度 {SA_0191}/99."
+    elif a_id == 452095387990229002:
+        states, plus = map(str, stu.split('+'))
+        plus = int(plus)
+        if states == "hp":
+            hp = HP_8199 + plus
+            HP_8199 = hp
+            msg1 = f"HPを+{plus}しました。"
+        elif states == "mp":
+            mp = MP_8199 + plus
+            MP_8199 = mp
+            msg1 = f"MPを+{plus}しました。"
+        elif states == "san":
+            san = SA_8199 + plus
+            SA_8199 = san
+            msg1 = f"SANを+{plus}しました。"
+        an = f"現在の鈴木 耕一のステータスを表示します。"
+        msg = f"鈴木 耕一\n耐久値 {HP_8199}/16. MP {MP_8199}/6. 正気度 {SA_8199}/99."
+    elif a_id == 556772231011631104:
+        pl_di, str1 = map(str, stu.split('&'))
+        states, plus = map(str, str1.split('+'))
+        plus = int(plus)
+        if pl_di == "0864":
+            if states == "hp":
+                hp = HP_0864 + plus
+                HP_0864 = hp
+                msg1 = f"HPを+{plus}しました。"
+            elif states == "mp":
+                mp = MP_0864 + plus
+                MP_0864 = mp
+                msg1 = f"MPを+{plus}しました。"
+            elif states == "san":
+                san = SA_0864 + plus
+                SA_0864 = san
+                msg1 = f"SANを+{plus}しました。"
+            an = f"現在の滝口 明夫のステータスを表示します。"
+            msg = f"滝口 明夫\n耐久値 {HP_0864}/12. MP {MP_0864}/11. 正気度 {SA_0864}/99."
+        elif pl_di == "0191":
+            if states == "hp":
+                hp = HP_0191 + plus
+                HP_0191 = hp
+                msg1 = f"HPを+{plus}しました。"
+            elif states == "mp":
+                mp = MP_0191 + plus
+                MP_0191 = mp
+                msg1 = f"MPを+{plus}しました。"
+            elif states == "san":
+                san = SA_0191 + plus
+                SA_0191 = san
+                msg1 = f"SANを+{plus}しました。"
+            an = f"現在の苑田 晋助を表示します。"
+            msg = f"苑田 晋助\n耐久値 {HP_0191}/12. MP {MP_0191}/11. 正気度 {SA_0191}/99."
+        elif pl_di_di == "8199":
+            if states == "hp":
+                hp = HP_8199 + plus
+                HP_8199 = hp
+                msg1 = f"HPを+{plus}しました。"
+            elif states == "mp":
+                mp = MP_8199 + plus
+                MP_8199 = mp
+                msg1 = f"MPを+{plus}しました。"
+            elif states == "san":
+                san = SA_8199 + plus
+                SA_8199 = san
+                msg1 = f"SANを+{plus}しました。"
+            an =f"現在の鈴木 耕一のステータスを表示します。"
+            msg = f"鈴木 耕一\n耐久値 {HP_8199}/16. MP {MP_8199}/6. 正気度 {SA_8199}/99."
+            
+    mention= f"<@{ctx.author.id}>"
+    embed = discord.Embed(title=an ,description=f"{msg1}\n結果:\n{msg}",color=discord.Colour.from_rgb(0,255,0))
+#     embed.set_author(name=firesult)
+    await ctx.send(f"{ctx.author.mention}")
+    await ctx.send(embed=embed)    
+        
+        
 # @bot.command(name="s0864")
 # async def s0864(ctx: str):
 #     global STR_0864
