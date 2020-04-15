28c1625b3f58ce88e0002e1f97572075707093d7
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-16 07:50:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -97,6 +97,21 @@ async def d(ctx, dice: str):
     await ctx.send(msg)
 
     
+@bot.command(name="h")
+async def dice(ctx: str):
+    """!h 短縮help"""
+    embed = discord.Embed(title="!d",description="!d {n}d{n}の書式で入力\n合計値のみ表示")
+    embed.add_field(name="!dice",value="!dice {n}d{n}の書式で入力\n配列表示あり",inline=False)
+    embed.add_field(name="!dj",value="!dj {n}d{n}<kの書式で入力",inline=False)
+    embed.add_field(name="!dp",value="!dj {n}d{n}+kの書式で入力",inline=False)
+    embed.add_field(name="!p",value="!p {states}+{N}の書式で入力\nkeeperは!p {id}&{states}+{N}の書式で入力\n{id}は各playerの#以降",inline=False)
+    embed.add_field(name="!m",value="!m {states}-{N}の書式で入力\nkeeperは!m {id}&{states}-{N}の書式で入力\n{id}は各playerの#以降",inline=False)
+    embed.add_field(name="!h",value="これを表示",inline=False)
+    embed.add_field(name="id",value="小沼さん\n0864\n\n六谷さん\n0191\n\n羅闇さん\n8199",inline=False)
+
+    await ctx.send(f"{ctx.author.mention}")
+    await ctx.send(embed=embed) 
+
 @bot.command(name="dice")
 async def dice(ctx, dice: str):
     """!dice {n}d{n}の書式で入力"""
