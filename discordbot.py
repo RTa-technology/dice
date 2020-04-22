79ea27dfbc75572c90bd31a49566f582db50ecb9
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-04-22 09:49:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -82,9 +82,9 @@ async def on_command_error(ctx, error):
 @bot.command(name="sushi")
 async def sushi(ctx: str):
     embed=discord.Embed(title="すし刃", color=0xffffff)
-    embed.add_field(name=" ", value="それは寿司の魂の衝突です。 高尾はクラスDの人物で、世界で最高のスシブレダーになることを目指し、現在もトレーニングを続けています。", inline=True)
-    embed.add_field(name=" ", value="彼は運命という渦に巻き込まれ、ダークスシとの戦いに身を投じます。彼は本当にDark Sushiを倒し、Sushi bladeの平和を取り戻すことができますか? ", inline=True)
-    embed.add_field(name=" ", value="風船", inline=True)
+    embed.add_field(name="　", value="それは寿司の魂の衝突です。 高尾はクラスDの人物で、世界で最高のスシブレダーになることを目指し、現在もトレーニングを続けています。", inline=True)
+    embed.add_field(name="　", value="彼は運命という渦に巻き込まれ、ダークスシとの戦いに身を投じます。彼は本当にDark Sushiを倒し、Sushi bladeの平和を取り戻すことができますか? ", inline=True)
+    embed.add_field(name="　", value="風船", inline=True)
     await ctx.send(embed=embed)
 
 
