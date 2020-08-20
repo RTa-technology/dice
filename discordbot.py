dbb9987296726aba4294a188eabd31616368a185
RTa_technology <57354947+RTa-technology@users.noreply.github.com>
2020-08-21 00:26:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -50,7 +50,39 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+    
+@bot.event
+async def on_raw_reaction_add(payload):
 
+    if payload.message_id == 746027056307110058:
+
+        print(payload.emoji.name)
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, bot.guilds)
+
+        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
+
+        if role is not None:
+            print(role.name + " was found!")
+            print(role.id)
+            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+            await member.add_roles(role)
+            print("done")
+
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+    if payload.message_id == 746027056307110058:
+        print(payload.emoji.name)
+
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g: g.id == guild_id, bot.guilds)
+        role = discord.utils.find(lambda r: r.name == payload.emoji.name, guild.roles)
+
+        if role is not None:
+            member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+            await member.remove_roles(role)
+            print("done")
 
 #@bot.event
 #async def on_ready():
