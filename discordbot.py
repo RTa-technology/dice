6271160de09e9f703b13147ce809850b62959fa4
RTa_technology <scpfoundationidcardmaker@gmail.com>
2020-08-26 01:28:52 +0900

    dispanderに差し戻し

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,7 @@ import discord
 import urllib.request
 import json
 import time
-from cogs.module import *
+from dispander.module import *
 from discord.ext import commands as rta
 bot = rta.Bot(command_prefix='!')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -48,8 +48,6 @@ SAN_4091 = 30
 #===============================================#
 @bot.event
 async def on_ready():
-    if message.author.bot:
-        return
     await dispand(message)
 
 @bot.event
