9ff0cad72fa532114ee97314c0b28e70414335d1
RTa_technology <scpfoundationidcardmaker@gmail.com>
2020-08-29 00:43:12 +0900

    mp3再生

--- discordbot.py
+++ discordbot.py
@@ -18,8 +18,8 @@ token_git = os.environ['GITHUB_API_TOKEN']
 #===============================================#
 #===============================================#
 #file___________________________________________#
-repository = "RTa-technology/dice"
-fileName = "save.txt"
+if not discord.opus.is_loaded():
+    discord.opus.load_opus("heroku-buildpack-libopus")
 
 
 
@@ -168,15 +168,52 @@ async def d(ctx:str):
 
 
 #===============================================#
-@bot.command(name="open")
-async def get_file(ctx:str):
-    global token_git
-    global repository
-    global fileName
-    g = Github(token_git)
-    repo = g.get_repo(repository)
-    contents = repo.get_contents(fileName)
-    await ctx.send(contents)
+@bot.command(aliases=["connect","summon"]) #connectやsummonでも呼び出せる
+async def join(ctx):
+    """Botをボイスチャンネルに入室させます。"""
+    voice_state = ctx.author.voice
+
+    if (not voice_state) or (not voice_state.channel):
+        await ctx.send("先にボイスチャンネルに入っている必要があります。")
+        return
+
+    channel = voice_state.channel
+
+    await channel.connect()
+    print("connected to:",channel.name)
+
+
+@bot.command(aliases=["disconnect","bye"])
+async def leave(ctx):
+    """Botをボイスチャンネルから切断します。"""
+    voice_client = ctx.message.guild.voice_client
+
+    if not voice_client:
+        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
+        return
+
+    await voice_client.disconnect()
+    await ctx.send("ボイスチャンネルから切断しました。")
+
+@bot.command()
+async def play(ctx):
+    """指定された音声ファイルを流します。"""
+    voice_client = ctx.message.guild.voice_client
+
+    if not voice_client:
+        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
+        return
+
+    if not ctx.message.attachments:
+        await ctx.send("ファイルが添付されていません。")
+        return
+
+    await ctx.message.attachments[0].save("tmp.mp3")
+
+    ffmpeg_audio_source = discord.FFmpegPCMAudio("tmp.mp3")
+    voice_client.play(ffmpeg_audio_source)
+
+    await ctx.send("再生しました。")
 #===============================================#
 
 
